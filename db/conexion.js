d54b6be4957450bafcf3ccf381afc772fe39b9b3
CesarCadena5 <cesarojitos555@hotmail.com>
2024-01-27 12:59:59 -0500

    verificando error de conexion db

--- db/conexion.js
+++ db/conexion.js
@@ -1,8 +1,9 @@
 import { connect } from "mongoose";
-import 'dotenv/config';
+// import 'dotenv/config';
 
 export const conexion = async () => {
     try {
+        console.log(process.env.COMPASS_CONNECTION);
         await connect(process.env.COMPASS_CONNECTION);
         console.log('db conectada');
     } catch (error) {
